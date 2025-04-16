ea1e3c0f35854b792d5957d852768321aed446b5
prateek041 <prateeksingh9741@gmail.com>
2025-04-17 02:11:53 +0530

    run http tracer example instead of XDP and cleanup

--- cmd/ebpf-o11y/main.go
+++ cmd/ebpf-o11y/main.go
@@ -4,14 +4,15 @@ import (
 	"fmt"
 	"log"
 
-	bpf "github.com/prateek041/ebpf-o11y/pkg/xdp"
+	bpf "github.com/prateek041/ebpf-o11y/pkg/http"
 )
 
 func main() {
 	fmt.Println("Starting the eBPF observability program...")
 
 	// list of programs to load
-	bpf.LoadXDP()
+	// bpf.LoadXDP()
+	bpf.LoadUProbe()
 
 	log.Println("Program Finished")
 }
